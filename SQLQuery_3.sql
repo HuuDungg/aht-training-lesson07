442222f23430a66e665a18d758f04088e47cbc3f
huudungg <huudung2004kt@gmail.com>
2024-11-22 08:23:10 +0700

    simple query

---
+++ SQLQuery_3.sql
@@ -0,0 +1,7 @@
+SELECT * FROM customers
+
+SELECT customerName, phone, city, country FROM customers
+
+SELECT * FROM customers WHERE customerName = 'Atelier Graphique'
+
+SELECT * FROM customers WHERE customername like '%A%'
