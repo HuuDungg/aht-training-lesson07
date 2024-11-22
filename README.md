0b4692d4da7e309f6e33ce9ffd019d2632d21eee
huudungg <huudung2004kt@gmail.com>
2024-11-22 08:22:29 +0700

    upload

---
+++ README.md
@@ -0,0 +1,7 @@
+\*Note:
+
+em chia các bài tập trong lesson ra thành các branch khác nhau. branch main sẽ chứa readme và mindmap
+
+\*Report:
+
+-Thanks a lot-
--- SQLQuery_3.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM customers
-
-SELECT customerName, phone, city, country FROM customers
-
-SELECT * FROM customers WHERE customerName = 'Atelier Graphique'
-
-SELECT * FROM customers WHERE customername like '%A%'
